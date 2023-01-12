c3d305361b2a6bb145be47fe41f6932a50b21475
hideba <baba.papa1120.ba@gmail.com>
2023-01-12 21:28:32 +0900

    impl repeat

--- src/combinators.test.ts
+++ src/combinators.test.ts
@@ -1,4 +1,4 @@
-import { not, or } from './combinators';
+import { not, or, cat, rep } from './combinators';
 import { char } from './char';
 import type { ParserOutput } from './types';
 
@@ -102,6 +102,202 @@ describe('or()', () => {
       const output = parser(input);
       expect(output).toEqual<ParserOutput<'a' | 'b'>>({
         result: 'fail'
+      });
+    });
+  });
+});
+
+
+describe('cat()', () => {
+  describe('cat([])', () => {
+    const parser = cat([]);
+
+    test('Empty input', () => {
+      const input = [] as const;
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<[]>>({
+        result: 'success',
+        data: [],
+        rest: []
+      });
+    });
+
+    test('Input "a"', () => {
+      const input = [...'a'];
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<[]>>({
+        result: 'success',
+        data: [],
+        rest: [...'a']
+      });
+    });
+  });
+
+  describe('cat([char("a"), char("b")])', () => {
+    const parser = cat([char('a'), char('b')]);
+
+    test('Empty input', () => {
+      const input = [] as const;
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<['a', 'b']>>({
+        result: 'fail'
+      });
+    });
+
+    test('Input "a"', () => {
+      const input = [...'a'];
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<['a', 'b']>>({
+        result: 'fail'
+      });
+    });
+
+    test('Input "abc"', () => {
+      const input = [...'abc'];
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<['a', 'b']>>({
+        result: 'success',
+        data: ['a', 'b'],
+        rest: [...'c']
+      });
+    });
+
+    test('Input "A"', () => {
+      const input = [...'A'];
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<['a', 'b']>>({
+        result: 'fail'
+      });
+    });
+  });
+});
+
+
+describe('rep()', () => {
+  describe('rep(char("a"))', () => {
+    const parser = rep(char('a'));
+
+    test('Empty input', () => {
+      const input = [] as const;
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<'a'[]>>({
+        result: 'success',
+        data: [],
+        rest: []
+      });
+    });
+
+    test('Input "a"', () => {
+      const input = [...'a'];
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<'a'[]>>({
+        result: 'success',
+        data: ['a'],
+        rest: []
+      });
+    });
+
+    test('Input "aa"', () => {
+      const input = [...'aa'];
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<'a'[]>>({
+        result: 'success',
+        data: ['a', 'a'],
+        rest: []
+      });
+    });
+
+    test('Input "aab"', () => {
+      const input = [...'aab'];
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<'a'[]>>({
+        result: 'success',
+        data: ['a', 'a'],
+        rest: [...'b']
+      });
+    });
+  });
+
+  describe('rep(char("a"), 1)', () => {
+    const parser = rep(char('a'), 1);
+
+    test('Empty input', () => {
+      const input = [] as const;
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<'a'[]>>({
+        result: 'fail'
+      });
+    });
+
+    test('Input "a"', () => {
+      const input = [...'a'];
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<'a'[]>>({
+        result: 'success',
+        data: ['a'],
+        rest: []
+      });
+    });
+
+    test('Input "aa"', () => {
+      const input = [...'aa'];
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<'a'[]>>({
+        result: 'success',
+        data: ['a', 'a'],
+        rest: []
+      });
+    });
+
+    test('Input "aab"', () => {
+      const input = [...'aab'];
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<'a'[]>>({
+        result: 'success',
+        data: ['a', 'a'],
+        rest: [...'b']
+      });
+    });
+  });
+
+  describe('rep(char("a"), 1, 2)', () => {
+    const parser = rep(char('a'), 1, 2);
+
+    test('Empty input', () => {
+      const input = [] as const;
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<'a'[]>>({
+        result: 'fail'
+      });
+    });
+
+    test('Input "a"', () => {
+      const input = [...'a'];
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<'a'[]>>({
+        result: 'success',
+        data: ['a'],
+        rest: []
+      });
+    });
+
+    test('Input "aa"', () => {
+      const input = [...'aa'];
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<'a'[]>>({
+        result: 'success',
+        data: ['a', 'a'],
+        rest: []
+      });
+    });
+
+    test('Input "aaa"', () => {
+      const input = [...'aaa'];
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<'a'[]>>({
+        result: 'success',
+        data: ['a', 'a'],
+        rest: [...'a']
       });
     });
   });
--- src/combinators.ts
+++ src/combinators.ts
@@ -1,4 +1,4 @@
-import type { Parser } from './types'
+import type { Parser, ParserData, ParserInput } from './types'
 
 type NotFunc = (p: Parser<unknown>) => Parser<null>;
 export const not: NotFunc = p => input => {
@@ -21,3 +21,63 @@ export const or: OrFunc = pList => input => {
       result: "fail",
     }
   }
+
+
+// type CatFunc = <T extends Parser<unknown>>() => () => any
+type CatFunc = <T extends Parser<unknown>[]>(ps: [...T]) => Parser<{ [K in keyof T]: ParserData<T[K]> }>;
+
+// //**
+//  * TはParser<unknow>[]型
+//  * psは例えば [Parser<"a">, Parser<"b">]みたいなタプル
+//  * 戻り値は例えば、
+//  * Parser<
+//  *  {
+//  *    0: ParserData<Parser<"a">> => "a"
+//  *    1: ParserData<Parser<"b">> => "b"
+//  *  }
+//     ... => Parser<["a", "b"]
+//  * >
+//  */
+//**
+
+export const cat: CatFunc = ps => input => {
+  const rs = [];
+  let i = input;
+  for (const p of ps) {
+    const r = p(i);
+    if (r.result === 'fail') return r;
+    rs.push(r.data);
+    i = r.rest;
+  }
+  return {
+    result: 'success',
+    data: rs as ParserData<ReturnType<ReturnType<CatFunc>>>,
+    rest: i
+  };
+};
+
+export type RepFunc = <T>(p: Parser<T>, min?: number,max?: number ) => Parser<T[]>
+
+export const rep: RepFunc = <T>(p: Parser<T>, min: number = 0, max: number = Number.MAX_SAFE_INTEGER) => (input:ParserInput) => {
+  if (min > max) throw new Error('rep: min > max is not allowed.');
+  if (min < 0) throw new Error('rep: negative min is not allowed.');
+  if (max < 0) throw new Error('rep: negative max is not allowed.');
+
+  let input2 = input
+  const rs: ParserData<typeof p>[] = []
+
+  for(let i = 0; i < max; i++){
+    const res = p(input2)
+    if (res.result === "fail")break
+    rs.push(res.data)
+    input2 = res.rest
+  }
+
+  if(rs.length < min)return {result: "fail"}
+
+  return {
+    result: "success",
+    data: rs,
+    rest: input2,
+  }
+}
